29e06bdb1e334e366c3b1e0c993004d058fdfa86
cx <enchantedgamerrblx@gmail.com>
2024-11-12 23:16:42 -0500

    Hello Fabric World!

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -18,7 +18,7 @@ public class ExampleMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		//Hello Fabric World!
 		LOGGER.info("Hello Fabric world!");
 	}
 }
